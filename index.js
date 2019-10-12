e79f19031581609bcfb909c8f3b8ba730570577c
AntiHero <prophreaker@gmail.com>
2019-10-12 22:52:19 +0300

    feat: implement auto mode + style add

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 let n = 0;
 let d = 0;
 let dSlider;
+let auto = true;
+
+// Resume button
+let btn = document.createElement("button");
+btn.innerHTML = "Pause";
+btn.className = "resume__btn";
+
+btn.onclick = function() {
+  auto = !auto;
+
+  if (auto) {
+    btn.innerHTML = "Pause";
+  } else {
+    btn.innerHTML = "Continue";
+  }
+};
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas().size(350, 350);
   angleMode(DEGREES);
-  dSlider = createSlider(1, 180, 1);
+  // dSlider = createSlider(1, 180, 1);
+
+  let cnv = document.getElementById("defaultCanvas0");
+  let footer = document.getElementsByClassName("footer")[0];
+  cnv.parentNode.insertBefore(btn, cnv.nextSibling);
+  btn.parentNode.insertBefore(footer, btn.nextSibling);
 }
 
 function draw() {
-  background(0);
-  translate(width/2, height/2);
+  background(46);
+  translate(width / 2, height / 2);
   stroke(255);
   // d = dSlider.value();
   noFill();
   beginShape();
   for (let i = 0; i < 361; i++) {
     let k = i * d;
-    let r = 150 * sin(n*k);
+    let r = 160 * sin(n * k);
     let x = r * cos(k);
     let y = r * sin(k);
     vertex(x, y);
@@ -25,18 +46,20 @@ function draw() {
   endShape();
 
   noFill();
-  stroke(0, 0, 255);
+  stroke(176, 0, 0);
   strokeWeight(2);
   beginShape();
   for (let i = 0; i < 361; i++) {
     let k = i;
-    let r = 150 * sin(n*k);
+    let r = 150 * sin(n * k);
     let x = r * cos(k);
     let y = r * sin(k);
     vertex(x, y);
   }
   endShape();
 
-  n += .01;
-  d += .01;
+  if (auto && d <= 361) {
+    n += 0.005;
+    d += 0.005;
+  }
 }
